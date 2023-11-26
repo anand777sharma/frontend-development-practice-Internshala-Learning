0518b48debea48fb0f89da6459b3270d900e7c0e
Anand Sharma <84133329+anand777sharma@users.noreply.github.com>
2023-11-26 22:43:09 +0530

    ..

---
+++ capSton_project_frontend-matrixmind777@gmail.com/script.js
@@ -0,0 +1,15 @@
+// getting email and password from the input field
+var email =document.getElementById('Input1').value;
+var password =document.getElementById('Password1').value;
+function check(){
+console.log(email, password);
+
+    // if (email=="admin@admin.com" || password=="12345")
+    // {
+    //     alert("login success!");
+    // }
+    // else{
+    //     alert("login failure!");
+    // } 
+}
+
